61b00d6294a7f087d18409feb69c0f0361ac6450
Vladimir <konsmanov-vova@mail.ru>
2021-02-22 00:11:29 +0300

    Добавил седьмое задание

--- js/task-7.js
+++ js/task-7.js
@@ -10,8 +10,8 @@
 //  * Можно положить либо снять деньги со счета.
 //  */
 const Transaction = {
-  DEPOSIT: 'deposit',
-  WITHDRAW: 'withdraw',
+ DEPOSIT: 'deposit',
+ WITHDRAW: 'withdraw',
 };
 
 /*
@@ -19,50 +19,43 @@ const Transaction = {
  */
 
 const account = {
-  // Текущий баланс счета
-  balance: 0,
-
-  // История транзакций
-  transactions: [],
-
-  /*
-   * Метод создает и возвращает объект транзакции.
-   * Принимает сумму и тип транзакции.
-   */
-  createTransaction(amount, type) {},
-
-  /*
-   * Метод отвечающий за добавление суммы к балансу.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций
-   */
-  deposit(amount) {},
-
-  /*
-   * Метод отвечающий за снятие суммы с баланса.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций.
-   *
-   * Если amount больше чем текущий баланс, выводи сообщение
-   * о том, что снятие такой суммы не возможно, недостаточно средств.
-   */
-  withdraw(amount) {},
-
-  /*
-   * Метод возвращает текущий баланс
-   */
-  getBalance() {},
-
-  /*
-   * Метод ищет и возвращает объект транзации по id
-   */
-  getTransactionDetails(id) {},
-
-  /*
-   * Метод возвращает количество средств
-   * определенного типа транзакции из всей истории транзакций
-   */
-  getTransactionTotal(type) {},
+  balance: 0, // Текущий баланс счета
+  transactions: [], // История транзакций
+  createTransaction(amount, type) {
+      let idTransaction = account.transactions.length+1                              // Метод создает и возвращает объект транзакции.
+      return { id: idTransaction, type: type, amount: amount,}                       // Принимает сумму и тип транзакции.
+  },
+  deposit(amount) {                                                                  // Метод отвечающий за добавление суммы к балансу.
+      this.transactions.push(this.createTransaction(amount, 'deposit'));       // Принимает сумму танзакции.
+      this.balance = this.balance + amount;                                          // Вызывает createTransaction для создания объекта транзакции
+  },                                                                                 // после чего добавляет его в историю транзакций
+  withdraw(amount) {                                                                    // Метод отвечающий за снятие суммы с баланса.
+      if (amount < this.balance) {                                                      // Принимает сумму танзакции.
+          this.transactions.push(this.createTransaction(amount, 'withdraw'));     // после чего добавляет его в историю транзакций.
+          this.balance = this.balance - amount;                                         // Вызывает createTransaction для создания объекта транзакции
+      } else {                                                                          // Если amount больше чем текущий баланс, выводи сообщение
+          console.log(`Снятие ${amount} не возможно, недостаточно средств.`);           // о том, что снятие такой суммы не возможно, недостаточно средств.
+      }
+  },
+  getBalance() {return this.balance},                                                    //Метод возвращает текущий баланс
+  getTransactionDetails(id) {
+      let result = {};
+      this.transactions.forEach(function (currentValue) {
+            if (currentValue.id === id){
+                result = currentValue;
+            }
+      })                                                                                  // Метод ищет и возвращает объект транзации по id
+      return result;
+  },
+  getTransactionTotal(type) {                                                           // Метод возвращает количество средств
+      let result = 0;                                                                   // определенного типа транзакции из всей истории транзакций
+      this.transactions.forEach(function (currentValue) {
+          if (currentValue.type === type){
+              result = result + currentValue.amount;
+          }
+      })
+      return result;
+  },
 };
+
+
